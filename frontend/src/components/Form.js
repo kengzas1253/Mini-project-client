c5130472bbb9868d061ff123f8692de45699474a
kengzas1253 <kengzas1253@gmail.com>
2020-04-12 19:58:41 +0700

    update 12/04/2020

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -23,6 +23,16 @@ const Form =()=>{
         getEmployees()
           
     }
+    const updateEmployee = async (employee_id) => {
+      await axios.put(`https://api-booking-parttimes.herokuapp.com/api/employees/${employee_id}`,form)
+       dispatch(
+           {type:'UPDATE_EMPLOYEE',
+          id: employee_id,
+           employee:{...form,id:  employee_id}
+       })
+       getEmployees()
+       
+     }
       const printEmployee = ()=>{
         if(employees && employees.length){
             return employees.map((employee,index)=>{
@@ -31,9 +41,13 @@ const Form =()=>{
                             
                             {employee.name}  {employee.surname  } : 
                             {employee.tel}  {employee.position}
-                            <button style={{marginLeft:"6px"}}
+                            <br/>
+                            <button style={{marginLeft:"6px"}} className="btn btn-danger btn-sm"
                             onClick={()=>deleteEmployee(employee.id)}
                             >Delete</button>
+                            <button style={{marginLeft:"6px" }} className="btn btn-info btn-sm"
+                            onClick={()=>updateEmployee(employee.id)}
+                            >Update</button>
                     </li> 
                 )
             })
@@ -45,9 +59,14 @@ const Form =()=>{
   return (
     <div>
         <Navbar/>
+        <div >
+        <h1 style={{marginTop:"6px",color:"red"}}>Edit Employee</h1>
         <ul>
                 {printEmployee()}
         </ul>
+
+        </div>
+
         <InputForm/>
     </div>
   );
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Login.css';
 
 const Login =()=>{
   return (
-    <div>
-      Login
-    </div>
+    <div class="body1">
+    <div class="wrapper">
+    <form class="form-signin">
+    <h2 class="form-signin-heading">Please login</h2>
+    <input type="text" class="form-control" name="username" placeholder="Email Address" />
+    <input type="password" class="form-control" name="password" placeholder="Password" />
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    </form>
+  </div>
+  </div>
   );
 }
 
